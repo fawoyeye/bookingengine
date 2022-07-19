4ac512c26c51fb20115113a28ec20194970625e2
fawoyeye <106695238+fawoyeye@users.noreply.github.com>
2022-07-19 22:45:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var hid =document.querySelector('.hid');
+	var one =document.querySelector('#one');		
+	var container =document.querySelector(".container");
+	var round =document.querySelector(".round");
+	var hotel =document.querySelector('.hotel');
+	var advanced = document.querySelector('.advanced');
+	var adButton = document.querySelector('.opt');
+	var car = document.querySelector('.car');
+			var multi = document.querySelector('.multi');
+	var multiple = document.querySelector('.multiple');
+var flightButton = document.querySelector('.add-form-button');
+var form = document.querySelector('.form-container');
+	var body = document.querySelector('body');
+	
+			flightButton.ondblclick = function(){
+							multiple.style.display='none';
+			}
+	
+	window.onload = function(){
+				container.style.display="block";		
+				hotel.style.display="none";
+				car.style.display="none";}
+			
+			
+			
+
+
+
+
+
+
